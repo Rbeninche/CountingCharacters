e2b81554acb07484f74d912aa6b0ef1aa4309c4f
Reginald Beninche <rbeninche@gmail.com>
2019-10-25 19:33:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Hello World";
+
+            Dictionary<char, int> wordsCount = new Dictionary<char, int>();
+
+            foreach (var item in sentence)
+            {
+                if (wordsCount.ContainsKey(item))
+                    wordsCount[item] += 1;
+
+                else
+                    wordsCount[item] = 1;
+
+
+            }
+
+
+            foreach(KeyValuePair<char, int> item in wordsCount)
+            {
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+                
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
